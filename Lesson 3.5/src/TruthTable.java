58e4d03c77c8ae10d111457f4dd4022c17fd96ad
Aspectious <griffin.siebert14@outlook.com>
2023-10-13 15:06:16 -0400

    Updated git, just a happy little commit.

--- Lesson 3.5/src/TruthTable.java
+++ Lesson 3.5/src/TruthTable.java
@@ -7,6 +7,25 @@ public class TruthTable
        int temperature = 90;
        boolean raining = false;
 
+
+       if (sunny && !raining) {
+    	   if (temperature > 85) {
+    		   System.out.println("It's Sunny outside with a temperature of " + temperature + ". Bring some sunscreen!");
+    	   } else if (temperature > 60 && temperature <=85) {
+    		   System.out.println("It's Sunny outside with a temperature of " + temperature + ". Perfect weather.");   
+    	   } else {
+    		   System.out.println("It's Sunny outside with a temperature of " + temperature + ". Wear a coat!");
+    	   }
+       } else if (raining && !sunny) {
+    	   System.out.println("It's Raining outside with a temperature of " + temperature + ". Bring an umbrella!");
+       } else if (raining && sunny) {
+    	   System.out.println("It's Raining outside with a temperature of " + temperature + ". Look out for Rainbows!");
+       } else {
+    	   System.out.println("It's Partly Cloudy outside with a temperature of " + temperature + ".");
+       }
+
+
+
        // Write an if statement for: If it's sunny,
        //  OR if the temperature is greater than 80
        //     and it's not raining, "Go to the beach!"
@@ -14,3 +33,4 @@ public class TruthTable
 
    }
 }
+
