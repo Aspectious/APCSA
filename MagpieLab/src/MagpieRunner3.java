2c20ab659cf292c664d90cd07551996b56c73334
Aspectious <griffin.siebert14@outlook.com>
2023-10-23 12:04:48 -0400

    heisenburger

--- MagpieLab/src/MagpieRunner3.java
+++ MagpieLab/src/MagpieRunner3.java
@@ -11,21 +11,24 @@ public class MagpieRunner3
 	/**
 	 * Create a Magpie, give it user input, and print its replies.
 	 */
+	
 	public static void main(String[] args)
 	{
-		
+		Magpie3 maggie = new Magpie3();
+		Scanner in = new Scanner(System.in);
 
+		// findKeworld Calls
+		
+		
 		
 		System.out.println (maggie.getGreeting());
-		
-
 		String statement = in.nextLine();
-		
 		while (!statement.equals("Bye"))
 		{
 			System.out.println (maggie.getResponse(statement));
 			statement = in.nextLine();
 		}
+		
 	}
 
 }
